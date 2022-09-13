36b3ce6890320d7a6ae326905b55671460497cf8
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-13 16:56:06 -0300

    refactor: validate sales body function

--- src/services/validations/validateSalesBody.js
+++ src/services/validations/validateSalesBody.js
@@ -1,20 +1,9 @@
-/* eslint-disable complexity */
-/* eslint-disable max-lines-per-function */
-const productsModel = require('../../models/products.model');
-
 const validateSalesBody = (body) => {
   const { productId, quantity } = body;
-  // for (let i = 0; i < body.length; i += 1) {
   if (!productId) {
-    return {
-      type: 'PRODUCT ID REQUIRED',
-      message: '"productId" is required',
-      status: 400,
-    };
+    return { type: 'PRODUCT ID REQUIRED', message: '"productId" is required', status: 400 };
   }
-  
   if (+quantity <= 0) {
-    console.log('opa');
     return {
       type: 'UNVALIABLE QUANTITY',
       message: '"quantity" must be greater than or equal to 1',
@@ -22,11 +11,7 @@ const validateSalesBody = (body) => {
     };
   }
   if (!quantity) {
-    return {
-      type: 'QUANTITY REQUIRED',
-      message: '"quantity" is required',
-      status: 400,
-    };
+    return { type: 'QUANTITY REQUIRED', message: '"quantity" is required', status: 400 };
   }
   return { type: null, message: '', status: 201 };
 };
