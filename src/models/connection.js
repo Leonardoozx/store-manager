dbd8abe98e60e322b8f837edfc9432feda0ef766
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-14 00:24:09 -0300

    refactor

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3306,
+  port: 3306,
   user: 'root',
   password: 'password',
   database: 'StoreManager',
