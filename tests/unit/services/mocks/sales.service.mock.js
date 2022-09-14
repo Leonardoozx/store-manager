c1dd6b12bb82ac613d2f145584ed2225d99a276c
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-14 01:42:05 -0300

    add mocks for sales services

--- tests/unit/services/mocks/sales.service.mock.js
+++ tests/unit/services/mocks/sales.service.mock.js
@@ -1,5 +1,5 @@
 const salesBodyWithoutProductId = {
-  quantity: 5
+  quantity: 5,
 };
 
 const salesBodyWithoutQuantity = {
@@ -8,17 +8,30 @@ const salesBodyWithoutQuantity = {
 
 const salesBodyWithWrongIntQuantity = {
   productId: 1,
-  quantity: 0
-}
+  quantity: 0,
+};
 
 const salesBodyWithRightValues = {
   productId: 2,
-  quantity: 5
+  quantity: 5,
 };
+
+const saleByIdCorrectMock = [
+  {
+    saleId: 1,
+    productId: 1,
+    quantity: 5,
+    date: "2022-09-14T03:21:59.000Z",
+  },
+];
+
+const saleByIdIncorrectMock = [];
 
 module.exports = {
   salesBodyWithWrongIntQuantity,
   salesBodyWithoutProductId,
   salesBodyWithoutQuantity,
   salesBodyWithRightValues,
+  saleByIdCorrectMock,
+  saleByIdIncorrectMock,
 };
